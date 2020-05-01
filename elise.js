12cc3876fa0cb50ee82e5a256e51ff02ec3ec7e7
lvise <lucas.batista@mail.tantto.com.br>
2020-05-01 16:05:06 -0300

    Playing youtube musics

--- elise.js
+++ elise.js
@@ -2,7 +2,8 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
 const streamOpt = { seek: 0, volume: 1 };
-const token = 'NzA1NTQyNDQ2MTA4NjM5MjUy.XqtPEw.raqwcG1E6EAWipZIDaV4cAqYog4';
+const token = 'NzA1NTQyNDQ2MTA4NjM5MjUy.XquUEw.zt44a2iAp0eFZJgfqiZEMdJb1rk';
+const search = require('yt-search');
 
 bot.login(token);
 
@@ -14,30 +15,25 @@ bot.on('message', async msg => {
     if (msg.author.bot) return;
 
     if (msg.content.toLowerCase().startsWith('?elise')) {
-        console.log('i read this')
+        const voiceChannel = msg.member.voice.channel;
+        if (!voiceChannel) return false;
 
         let content = msg.content;
         let newcontent = content.split(' ');
-
-        console.log(newcontent)
-
-        let voiceChannel = msg.member.voice.channel;
-        if (!voiceChannel) return false;
-
+        if (newcontent[1] == 'procurar') search(newcontent[2])
         if (newcontent[1] == 'stop') { voiceChannel.leave() }
 
         voiceChannel.join().then(conn => {
-            console.log('connected')
             const stream = ytdl(newcontent[1], { filter: 'audioonly' });
+            const player = conn.play(stream, streamOpt)
 
-            const player = conn.play(stream)
-
-            player.on('start', () => {
-                console.log('startou')
-            })
             player.on('end', () => {
                 voiceChannel.leave();
             })
+
         }).catch(console.error);
     }
+
+
 })
+
