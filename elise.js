99373fb818e8d15b026d57fddfdec0a13cca2a4f
lvise <lucas.batista@mail.tantto.com.br>
2020-05-03 22:40:05 -0300

    Finalizado

--- elise.js
+++ elise.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
 const streamOpt = { seek: 0, volume: 1 };
-const token = 'NzA1NTQyNDQ2MTA4NjM5MjUy.XquUEw.zt44a2iAp0eFZJgfqiZEMdJb1rk';
-const search = require('yt-search');
+const token = 'NzA1NTQyNDQ2MTA4NjM5MjUy.XqxzJA.Xz4B4A_DBpwtKDDDwTwReBzv_sg';
+
 
 bot.login(token);
 
@@ -20,20 +20,18 @@ bot.on('message', async msg => {
 
         let content = msg.content;
         let newcontent = content.split(' ');
-        if (newcontent[1] == 'procurar') search(newcontent[2])
+
         if (newcontent[1] == 'stop') { voiceChannel.leave() }
+        if (newcontent[1].toString().startsWith('http')) {
+            voiceChannel.join().then(conn => {
+                const stream = ytdl(newcontent[1], { filter: 'audioonly' });
+                const player = conn.play(stream, streamOpt)
 
-        voiceChannel.join().then(conn => {
-            const stream = ytdl(newcontent[1], { filter: 'audioonly' });
-            const player = conn.play(stream, streamOpt)
+                player.on('end', () => {
+                    voiceChannel.leave();
+                })
 
-            player.on('end', () => {
-                voiceChannel.leave();
-            })
-
-        }).catch(console.error);
+            }).catch(console.error);
+        }
     }
-
-
 })
-
